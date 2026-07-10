c07bac6a13fd3cb7e294a38b06701b5fc637404f
srihariharan9102004 <srihariharan9102004@gmail.com>
2026-07-10 16:05:17 +0530

    Add Bootstrap dependency

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
